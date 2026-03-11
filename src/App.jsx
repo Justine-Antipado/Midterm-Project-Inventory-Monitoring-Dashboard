7eae47054e701ac663c141472b47d64cd186c171
Justine-Antipado <justineantipado10@gmail.com.com>
2026-03-11 15:21:28 +0800

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import imslogo from './assets/inventory.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  // State to track which "page" we are looking at
+  const [activeTab, setActiveTab] = useState("home");
 
   return (
     <>
-      <div>
+      {/* Navigation bar/header 
+    <div className='nav-bar'>
+      <h1>Inventory Monitoring Dashboard</h1>
+      <div className='tabs-btn'>
+        <button></button>
+        <button>Product</button>
+      </div>
+    </div>*/}
+
+      <div className="nav-bar">
+        <div className="logo">
+          <img src={imslogo} className="logo" alt="Inventory Monitoring System Logo" />
+          <h1>Inventory Monitoring Dashboard</h1>
+        </div>
+        
+        <div className="tabs-btn">
+          {/* Change state on click to switch views */}
+          <button
+            className={activeTab === "home" ? "active" : ""}
+            onClick={() => setActiveTab("home")}
+          >
+            Home
+          </button>
+          <button
+            className={activeTab === "product" ? "active" : ""}
+            onClick={() => setActiveTab("product")}
+          >
+            Product
+          </button>
+        </div>
+      </div>
+
+      {/*<div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +60,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p>*/}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
